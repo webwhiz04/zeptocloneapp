c53c809ee744e867210242b61d43bd6ab9531a14
preetham <preethampoojary99@gmai.com>
2026-05-07 17:22:36 +0530

    fix vercel routing

--- backend/config/config.js
+++ backend/config/config.js
@@ -4,13 +4,13 @@ dotenv.config();
 const config = {
     PORT: process.env.PORT || 5000,
     MONGO_URI: process.env.MONGO_URI,
-    EMAIL_USER: process.env.EMAIL_USER,
-    EMAIL_PASS: process.env.EMAIL_PASS,
+    EMAIL_USER: process.env.EMAIL_USER || "",
+    EMAIL_PASS: process.env.EMAIL_PASS || "",
     EMAIL_FROM: process.env.EMAIL_FROM,
     
     RAZORPAY_KEY_ID: process.env.RAZORPAY_KEY_ID,
     RAZORPAY_KEY_SECRET: process.env.RAZORPAY_KEY_SECRET,
-    NODE_ENV: process.env.NODE_ENV || "development",
+    NODE_ENV: process.env.NODE_ENV || "production",
 };
 
 export default config;
